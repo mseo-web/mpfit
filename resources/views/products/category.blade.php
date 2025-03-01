f9b10030f0ae5f4fdc0222529f47b5c6d88f915c
mseo-web <mihas-ox7-74@mail.ru>
2025-03-01 09:59:30 +0500

    Some changes

--- resources/views/products/category.blade.php
+++ resources/views/products/category.blade.php
@@ -10,20 +10,32 @@
         @if($category->products->isNotEmpty())
         <div class="b-example-divider"></div>
         <h2>Список товаров категории</h2>
-            @foreach ($category->products as $product)
-            <div class="card mb-4">
-                <div class="card-header">
-                    <h2><a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></h2>
-                    <p>Цена: {{ $product->price }} руб.</p>
+        <div class="mt-5 row">
+            @foreach($category->products as $product)
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <h3><a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></h3>
+                            <h4> - {{ $product->price }} руб.</h4>
+                            <p>Опубликовано: {{ $product->created_at }}</p>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">{!! $product->description !!}</p>
+                            <hr>
+                            <h5>Категории:</h5>
+                            <ul>
+                                <li>{{ $product->category->name }}</li>
+                            </ul>
+                            <form action="{{ route('orders.create') }}" method="GET">
+                                <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                <button type="submit">Заказать</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <p class="card-text">{!! $product->description !!}</p>
-                    <hr>
-                    <h5>Категория:</h5>
-                    <p>{{ $product->category->name }}</p>
-                </div>
-            </div>
             @endforeach
+        </div>
+
         @else
         <div class="fw-semibold fst-italic text-info fs-3 mt-3">В этой категории пока нет товаров.</div>
         @endif
